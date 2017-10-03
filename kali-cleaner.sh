6e9a272908f717840c7baa7ebfa5c6483e03932f
Andre.my.id <GitHub@andre.my.id>
2017-10-03 23:20:36 +0700

    Update kali-cleaner.sh

--- kali-cleaner.sh
+++ kali-cleaner.sh
@@ -1,66 +1,78 @@
 #!/bin/bash
-YELLOW="\033[1;33m"
-RED="\033[0;31m"
-ENDCOLOR="\033[0m"
 
-echo 
-echo $RED"==================== Kali-Cleaner ===================="$ENDCOLOR
-echo $RED"=           Cleaner for Kali by Andre myID           ="$ENDCOLOR
-echo $RED"======================================================"$ENDCOLOR
-echo 
+# Bash Color : \[\033[ <code>m\] \]
+#  Black=         "\033[0;30m"     DarkGray=      "\033[1;30m"
+#  Blue=          "\033[0;34m"     LightBlue=     "\033[1;34m"
+#  Green=         "\033[0;32m"     LightGreen=    "\033[1;32m"
+#  Cyan=          "\033[0;36m"     LightCyan=     "\033[1;36m"
+#  Red=           "\033[0;31m"     LightRed=      "\033[1;31m"
+#  Purple=        "\033[0;35m"     LightPurple=   "\033[1;35m"
+#  Brown=         "\033[0;33m"     Yellow=        "\033[1;33m"
+#  LightGray=     "\033[0;37m"     White=         "\033[1;37m"
+#  End="\033[0m"
 
-OLDCONF=$(dpkg -l|grep "^rc"|awk '{print $2}')
-CURKERNEL=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
-LINUXPKG="linux-(image|headers|debian-modules|restricted-modules)"
-METALINUXPKG="linux-(image|headers|restricted-modules)-(generic|i386|server|common|rt|xen)"
-#OLDKERNELS=$(dpkg -l|awk '{print $2}'|grep -E $LINUXPKG |grep -vE $METALINUXPKG|grep -v $CURKERNEL)
+LightCyan="\033[1;36m"
+LightGreen="\033[1;32m"
+LightRed="\033[1;31m"
+Yellow="\033[1;33m"
+End="\033[0m"
 
-if [ $USER != root ]; then
-echo $RED"[Kali-cleaner]:Error: must be root"
-echo $YELLOW"[Kali-cleaner]:Exiting..."$ENDCOLOR
-exit 0
-fi
-
-echo $YELLOW"[Kali-cleaner]:Apt install aptitude..."$ENDCOLOR
-sudo apt install aptitude
+echo
+echo $LightRed"==================== "$LightCyan"Kali-Cleaner"$LightRed" ===================="
+echo "=           "$Yellow"Cleaner for Kali by Andre myID"$LightRed"           ="
+echo "======================================================"$End
 echo
 
-echo $YELLOW"[Kali-cleaner]:Cleaning apt cache..."$ENDCOLOR
+OldConf=$(dpkg -l|grep "^rc"|awk '{print $2}')
+CurKernel=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
+LinuxPkg="linux-(image|headers|debian-modules|restricted-modules)"
+MetaLinuxPkg="linux-(image|headers|restricted-modules)-(generic|i386|server|common|rt|xen)"
+#OldKernels=$(dpkg -l|awk '{print $2}'|grep -E $LinuxPkg |grep -vE $MetaLinuxPkg|grep -v $CurKernel)
+
+if [ $USER != root ]; then
+    echo $LightRed"[Kali-cleaner]:Error: must be root"
+    echo $Yellow"[Kali-cleaner]:Exiting..."$End
+    exit 0
+fi
+
+echo $LightRed"[Kali-cleaner]:"$Yellow"Apt install aptitude..."$End
+sudo apt install aptitude -y
+echo
+
+echo $LightRed"[Kali-cleaner]:"$Yellow"Cleaning apt cache..."$End
 sudo aptitude clean
 echo
 
-echo $YELLOW"[Kali-cleaner]:Removing old config files..."$ENDCOLOR
-sudo aptitude purge $OLDCONF
+echo $LightRed"[Kali-cleaner]:"$Yellow"Removing old config files..."$End
+sudo aptitude purge $OldConf
 
-echo $YELLOW"[Kali-cleaner]:Removing old kernels..."$ENDCOLOR
-sudo aptitude purge $OLDKERNELS
+echo $LightRed"[Kali-cleaner]:"$Yellow"Removing old kernels..."$End
+sudo aptitude purge $OldKernels
 
-echo $YELLOW"[Kali-cleaner]:Emptying every trashes..."$ENDCOLOR
+echo $LightRed"[Kali-cleaner]:"$Yellow"Emptying every trashes..."$End
 rm -rf /home/*/.local/share/Trash/*/** &> /dev/null
 rm -rf /root/.local/share/Trash/*/** &> /dev/null
 echo
 
-echo $YELLOW"[Kali-cleaner]:Check RAM free -h"$ENDCOLOR
+echo $LightRed"[Kali-cleaner]:"$Yellow"Check RAM free -h"$End
 sudo free -h
 echo
 
-#echo $YELLOW"[Kali-cleaner]:Clear PageCache only"$ENDCOLOR
+#echo $LightRed"[Kali-cleaner]:"$Yellow"Clear PageCache only"$End
 #sudo sync; echo 1 > /proc/sys/vm/drop_caches && free -h
 #echo
 
-#echo $YELLOW"[Kali-cleaner]:Clear Dentries and inodes"$ENDCOLOR
+#echo $LightRed"[Kali-cleaner]:"$Yellow"Clear Dentries and inodes"$End
 #sudo sync; echo 2 > /proc/sys/vm/drop_caches && free -h
 #echo
 
-echo $YELLOW"[Kali-cleaner]:Clear PageCache, dentries and inodes"$ENDCOLOR
+echo $LightRed"[Kali-cleaner]:"$Yellow"Cleaning PageCache, dentries and inodes"$End
 sudo sync; echo 3 > /proc/sys/vm/drop_caches && free -h
 echo
 
-echo $YELLOW"[Kali-cleaner]:Clear Swap Space"$ENDCOLOR
+echo $LightRed"[Kali-cleaner]:"$Yellow"Cleaning Swap Space"$End
 sudo swapoff -a && swapon -a && free -h
 echo
 
-echo $YELLOW"[Kali-cleaner]:Script Finished!"$ENDCOLOR
-echo 
-echo $RED"Goodbye..... ^.^v"$ENDCOLOR
-echo 
+echo $LightRed"[Kali-cleaner]:"$LightGreen"Kali-Cleaner Finished!"$End" ^.^v"
+echo
